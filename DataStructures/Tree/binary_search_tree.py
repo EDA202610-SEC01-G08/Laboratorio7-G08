ad6dbf8840ed6ebacee31f92eb316556471f40d5
zeta976 <a.zuluagar@uniandes.edu.co>
2026-04-10 07:12:39 -0500

    bst.py

---
+++ DataStructures/Tree/binary_search_tree.py
@@ -0,0 +1,13 @@
+import bst_node as bst_node
+def new_map():
+    return {"root": None}
+
+def put(my_bst, key, value):
+    """
+    Agrega un nuevo nodo al árbol binario. Si la llave ya existe, se actualiza el valor del nodo.
+    """
+    
+    
+    
+    
+    
